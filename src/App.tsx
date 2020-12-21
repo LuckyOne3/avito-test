e017f31a5450f07921ed3b193a9465aceb7d8d9e
Алексей <alex@loc.local>
2020-12-21 19:25:40 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {FormInputs} from "./components/formInputs/formInputs";
 import {Previewer} from "./components/previewer/previewer";
